96e5493ae19f59e57e3ff59ce0da7e7a8e85fa12
Galambos Eniko <g.eniko@ding.hu>
2021-09-02 13:36:21 +0200

    Add gsap

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom'
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
-  return (<h1>Hello</h1>)
+  useEffect(() => {
+    gsap.to("#title", {color: 'red', duration: 3})
+  })
+
+  return (<h1 id="title">Hello</h1>)
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
