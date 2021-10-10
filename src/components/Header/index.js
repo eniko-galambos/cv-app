e4c50133f0917c745e51a9c9b754644c247d8e10
Galambos Eniko <g.eniko@ding.hu>
2021-10-10 21:20:01 +0200

    feat: Add title animations

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,18 +2,16 @@ import React, { useEffect } from 'react';
 import { gsap } from 'gsap';
 
 const Header = () => {
-  useEffect(() => {
-    gsap.set('#title', {
-      y: 10,
-      x: 10,
-    });
-
+  // Animations
+  const floatTitle = () => {
     gsap.from('#title', {
       x: -50,
       opacity: 0,
       duration: 1.5,
     });
+  };
 
+  const lowerTitle = () => {
     gsap.to('#title', {
       scale: 0.3,
       y: 0,
@@ -26,13 +24,63 @@ const Header = () => {
       },
       transformOrigin: 'top left',
     });
+  };
+
+  const hoverTitle = (direction) => {
+    if (direction === 'front') {
+      gsap.to('#titleLine', {
+        scaleX: 2,
+        transformOrigin: 'left',
+        duration: 0.2,
+      });
+      gsap.to('#titleLabel', {
+        color: '#14142d',
+        opacity: 0.7,
+        duration: 0.2,
+      });
+    } else {
+      gsap.to('#titleLine', {
+        scaleX: 1,
+        duration: 0.2,
+      });
+      gsap.to('#titleLabel', {
+        color: 'black',
+        opacity: 1,
+        duration: 0.2,
+      });
+    }
+  };
+
+  // Hooks
+  useEffect(() => {
+    gsap.set('#title', {
+      y: 10,
+      x: 10,
+    });
+
+    floatTitle();
+    lowerTitle();
   });
 
   return (
     <header className="fixed right-5 top-5 left-5 flex justify-between z-10">
-      <h1 id="title" className="text-8xl font-extralight uppercase">
-        Enikő Galambos
-      </h1>
+      <div id="title">
+        <a
+          onMouseOver={(event) => hoverTitle('front')}
+          onFocus={(event) => hoverTitle('front')}
+          onMouseOut={(event) => hoverTitle('back')}
+          onBlur={(event) => hoverTitle('back')}
+          href="#"
+        >
+          <h1
+            id="titleLabel"
+            className="text-8xl font-extralight uppercase cursor-pointer"
+          >
+            Enikő Galambos
+          </h1>
+        </a>
+        <div id="titleLine" className="h-0.5 w-20 bg-black mt-3 ml-2"></div>
+      </div>
       <div className="flex">
         <p>HU</p>
         <p className="ml-2">Download</p>
