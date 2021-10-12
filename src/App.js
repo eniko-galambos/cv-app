a8b98f4f10801deed7b5927f9d6128798f8c3aa9
Galambos Eniko <g.eniko@ding.hu>
2021-10-12 16:37:17 +0200

    feat: Rearrange assets, add profile image

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import Hero from './components/Hero';
 import Header from './components/Header';
+import profileFront from './assets/images/profile_front.jpg';
+import profileBack from './assets/images/profile_back.jpg';
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -12,6 +14,20 @@ const App = () => {
     <div>
       <Header />
       <Hero />
+      <div id="#images" className="relative">
+        <img
+          src={profileBack}
+          className="absolute left-48 opacity-50"
+          alt="Profile of me"
+          style={{ width: 700 }}
+        ></img>
+        <img
+          src={profileFront}
+          className="absolute left-72 -top-28"
+          alt="Profile of me"
+          style={{ width: 500 }}
+        ></img>
+      </div>
     </div>
   );
 };
