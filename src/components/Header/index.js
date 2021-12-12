5505645d58fc850cd70ea19b3d553001c04a196f
Galambos Eniko <g.eniko@ding.hu>
2021-12-12 20:17:23 +0100

    feat: Header logo style

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,8 +4,9 @@ import { gsap } from 'gsap';
 const Header = () => {
   return (
     <header className="fixed right-5 top-5 left-5 flex justify-between z-10">
-      <a href="#" className="font-newyork">
-        Enikő Galambos
+      <a href="#" className="font-newyork relative">
+        <span className="text-3xl absolute">Enikő</span>
+        <span className="text-xl absolute top-5 left-5">Galambos</span>
       </a>
     </header>
   );
