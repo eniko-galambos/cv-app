3c4bb3dbc9d42ddd0103605bc376a2d18b9f4a25
Galambos Eniko <g.eniko@ding.hu>
2021-12-12 22:54:38 +0100

    feat: Add logo reveal animation

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,9 +2,28 @@ import React, { useEffect } from 'react';
 import { gsap } from 'gsap';
 
 const Header = () => {
+  // Animations
+  const revealLogo = () => {
+    gsap.fromTo(
+      '#logo',
+      {
+        opacity: 0,
+      },
+      {
+        opacity: 1,
+        duration: 1.5,
+      },
+    );
+  };
+
+  // Hooks
+  useEffect(() => {
+    revealLogo();
+  });
+
   return (
     <header className="fixed right-5 top-5 left-5 flex justify-between z-10">
-      <a href="#" className="font-newyork relative">
+      <a id="logo" href="#" className="font-newyork relative">
         <span className="text-3xl absolute">Enikő</span>
         <span className="text-xl absolute top-5 left-5">Galambos</span>
       </a>
