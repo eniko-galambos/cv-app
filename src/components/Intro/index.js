60a29369d38f771b78339e7a5b3c5103054cbf08
Galambos Eniko <g.eniko@ding.hu>
2021-12-28 19:27:39 +0100

    feat: Add intro animations

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -1,17 +1,74 @@
-import React from 'react';
+import gsap from 'gsap';
+import React, { useEffect } from 'react';
 import profileImg from '../../assets/images/profile.jpg';
 
 const Intro = () => {
+  const titleScrolltl = gsap.timeline({
+    scrollTrigger: {
+      trigger: '#intro-title',
+      toggleActions: 'play none none none',
+      start: 'center 90%',
+    },
+  });
+
+  // Animations
+  const revealTitle = () => {
+    return gsap.from('#intro-title', {
+      y: 60,
+      opacity: 0,
+      duration: 1,
+    });
+  };
+
+  const revealSubtitle = () => {
+    return gsap.from('#intro-subtitle', {
+      y: 60,
+      opacity: 0,
+      duration: 1,
+    });
+  };
+
+  const moveFadedTitle = () => {
+    gsap.fromTo(
+      '#intro-faded-title',
+      { y: 150 },
+      {
+        y: -150,
+        scrollTrigger: {
+          trigger: '#intro',
+          toggleActions: 'restart pause reverse pause',
+          start: 'top 100%',
+          scrub: 0.4,
+        },
+      },
+    );
+  };
+
+  // Hooks
+  useEffect(() => {
+    titleScrolltl.add(revealTitle());
+    titleScrolltl.add(revealSubtitle(), 0.5);
+
+    moveFadedTitle();
+  });
+
   return (
-    <section className="relative">
+    <section id="intro" className="relative">
       <div className="relative mt-60 mb-40 text-center w-full">
         <div className="absolute flex justify-center w-full -top-28 md:-top-16 lg:-top-24">
-          <p className="font-newyork text-6xl md:text-7xl lg:text-9xl opacity-5">
+          <p
+            id="intro-faded-title"
+            className="font-newyork text-6xl md:text-7xl lg:text-9xl opacity-5"
+          >
             Frontend web developer
           </p>
         </div>
-        <h2 className="font-newyork text-4xl">Frontend web developer</h2>
-        <h3 className="font-fashion text-xs">(and hobby chef & artist)</h3>
+        <h2 id="intro-title" className="font-newyork text-4xl">
+          Frontend web developer
+        </h2>
+        <h3 id="intro-subtitle" className="font-fashion text-xs">
+          (and hobby chef & artist)
+        </h3>
       </div>
       <div className="bg-gray-100 py-60 ">
         <div className="relative flex flex-col items-center">
